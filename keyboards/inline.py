ad821564777186dc9c2f122a135660cb9e2677e2
Your Name <you@example.com>
2025-10-27 14:59:40 +0300

    added button "Как идет обучение"

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -1,31 +1,35 @@
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 from data.loader import manager
 
 
 def start_menu():
     markup = InlineKeyboardMarkup(row_width=True)
     markup.row(
-        InlineKeyboardButton(text="Часто задаваемые вопросы (FAQ)", callback_data=f"faq"),
+        InlineKeyboardButton(
+            text="Часто задаваемые вопросы (FAQ)", callback_data="faq"
+        ),
+        InlineKeyboardButton(
+            text="Как проходит обучение", callback_data="how_learning_goes"
+        ),
     )
     markup.row(
-        InlineKeyboardButton(text="Телефонный справочник", callback_data=f"telephone"),
-        InlineKeyboardButton(text="Задать вопрос", callback_data=f"ask")
+        InlineKeyboardButton(text="Телефонный справочник", callback_data="telephone"),
+        InlineKeyboardButton(text="Задать вопрос", callback_data="ask"),
     )
     markup.row(
-        InlineKeyboardButton(text="Профиль", callback_data=f"profile"),
-        InlineKeyboardButton(text="Правила", callback_data=f"rules")
+        InlineKeyboardButton(text="Профиль", callback_data="profile"),
+        InlineKeyboardButton(text="Правила", callback_data="rules"),
     )
     return markup
 
 
 def start_administrator_menu():
     markup = InlineKeyboardMarkup(row_width=True)
+    markup.row(InlineKeyboardButton(text="Ответить на вопрос", callback_data="answer"))
     markup.row(
-        InlineKeyboardButton(text="Ответить на вопрос", callback_data=f"answer")
-    )
-    markup.row(
-        InlineKeyboardButton(text="Профиль", callback_data=f"profile"),
-        InlineKeyboardButton(text="Правила", callback_data=f"rules")
+        InlineKeyboardButton(text="Профиль", callback_data="profile"),
+        InlineKeyboardButton(text="Правила", callback_data="rules"),
     )
     return markup
 
@@ -33,36 +37,38 @@ def start_administrator_menu():
 def show_static_question_category():
     markup = InlineKeyboardMarkup(row_width=True)
     categories = manager.question_category.get_category()
-    buttons = [InlineKeyboardButton(text=category[0], callback_data=f"category_{category[0]}") for
-               category in categories]
+    buttons = [
+        InlineKeyboardButton(text=category[0], callback_data=f"category_{category[0]}")
+        for category in categories
+    ]
     markup.add(*buttons)
-    markup.add(
-        InlineKeyboardButton(text="Назад", callback_data=f"main")
-    )
+    markup.add(InlineKeyboardButton(text="Назад", callback_data="main"))
     return markup
 
 
 def show_dynamic_question_category():
     markup = InlineKeyboardMarkup(row_width=True)
     categories = manager.question_category.get_category()
-    buttons = [InlineKeyboardButton(text=category[0], callback_data=f"ctg_{category[0]}") for
-               category in categories]
+    buttons = [
+        InlineKeyboardButton(text=category[0], callback_data=f"ctg_{category[0]}")
+        for category in categories
+    ]
     markup.add(*buttons)
-    markup.add(
-        InlineKeyboardButton(text="Назад", callback_data=f"main")
-    )
+    markup.add(InlineKeyboardButton(text="Назад", callback_data="main"))
     return markup
 
 
 def show_category_answer():
     markup = InlineKeyboardMarkup(row_width=True)
     categories = manager.question_category.get_category()
-    buttons = [InlineKeyboardButton(text=category[0], callback_data=f"acategory_{category[0]}") for
-               category in categories]
+    buttons = [
+        InlineKeyboardButton(text=category[0], callback_data=f"acategory_{category[0]}")
+        for category in categories
+    ]
     markup.add(*buttons)
     markup.add(
-        InlineKeyboardButton(text="Все категории", callback_data=f"allcateg"),
-        InlineKeyboardButton(text="Назад", callback_data=f"main")
+        InlineKeyboardButton(text="Все категории", callback_data="allcateg"),
+        InlineKeyboardButton(text="Назад", callback_data="main"),
     )
     return markup
 
@@ -74,46 +80,60 @@ def show_category_questions(category_name):
         text1 = " ".join(question[0:2])
         text1 += " " + question[3]
         # text1 += " " + question[6][:50]
-        markup.add(
-            InlineKeyboardButton(text=text1, callback_data=f"?")
-        )
-    markup.add(
-        InlineKeyboardButton(text="Назад", callback_data=f"main")
-    )
+        markup.add(InlineKeyboardButton(text=text1, callback_data="?"))
+    markup.add(InlineKeyboardButton(text="Назад", callback_data="main"))
     return markup
 
 
 def show_subcategories(category_id):
     markup = InlineKeyboardMarkup(row_width=True)
-    subcategories = manager.question_subcategory.get_subcategories_by_category_id(category_id)
-    buttons = [InlineKeyboardButton(text=subcategory[1], callback_data=f"subcat_{subcategory[0]}") for subcategory in subcategories]
-    markup.add(*buttons)
-    markup.add(
-        InlineKeyboardButton(text="Назад", callback_data=f"faq")
+    subcategories = manager.question_subcategory.get_subcategories_by_category_id(
+        category_id
     )
+    buttons = [
+        InlineKeyboardButton(
+            text=subcategory[1], callback_data=f"subcat_{subcategory[0]}"
+        )
+        for subcategory in subcategories
+    ]
+    markup.add(*buttons)
+    markup.add(InlineKeyboardButton(text="Назад", callback_data="faq"))
     return markup
 
 
 def back_main():
     markup = InlineKeyboardMarkup(row_width=True)
-    markup.add(
-        InlineKeyboardButton(text="Назад", callback_data=f"main")
-    )
+    markup.add(InlineKeyboardButton(text="Назад", callback_data="main"))
     return markup
 
 
 def back_static_categories():
     markup = InlineKeyboardMarkup(row_width=True)
-    markup.add(
-        InlineKeyboardButton(text="Назад", callback_data=f"faq")
-    )
+    markup.add(InlineKeyboardButton(text="Назад", callback_data="faq"))
     return markup
 
 
 def back_dynamic_categories():
     markup = InlineKeyboardMarkup(row_width=True)
+    markup.add(InlineKeyboardButton(text="Назад", callback_data="ask"))
+    return markup
+
+
+def how_to_learn_goes_markup():
+    markup = InlineKeyboardMarkup(row_width=True)
     markup.add(
-        InlineKeyboardButton(text="Назад", callback_data=f"ask")
+        InlineKeyboardButton(text="Лекции", callback_data="how_learning_goes_lections"),
+        InlineKeyboardButton(
+            text="Семинарские занятия", callback_data="how_learning_goes_seminars"
+        ),
+        InlineKeyboardButton(text="ЛМС система", callback_data="how_learning_goes_lms"),
+        InlineKeyboardButton(text="Назад", callback_data="how_learning_goes"),
+        row_width=1,
     )
     return markup
 
+
+def back_how_to_learn_goes():
+    markup = InlineKeyboardMarkup(row_width=True)
+    markup.add(InlineKeyboardButton(text="Назад", callback_data="how_learning_goes"))
+    return markup
---
+++ main.py
@@ -0,0 +1,9 @@
+from data.loader import bot
+
+
+def main():
+    bot.infinity_polling()
+
+
+if __name__ == "__main__":
+    main()
